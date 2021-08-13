ba64dfca7f299d908b31db58e5fc05de2006ba7f
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-13 10:26:27 +0530

    feat:Ability To Find Maximum Float

--- FindMaximumNum/Check.cs
+++ FindMaximumNum/Check.cs
@@ -24,5 +24,22 @@ namespace FindMaximumNum
             throw new Exception("firstValue,secondValue,Thirdvalue are same !");
 
         }
+        public static float MaximumFloat(float firstValue, float secondValue, float thirdValue)
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,ThirdValue are same!");
+        }
     }
 }
--- FindMaximumNum/Program.cs
+++ FindMaximumNum/Program.cs
@@ -11,6 +11,9 @@ namespace FindMaximumNum
             Console.WriteLine(" Maximum Number is:-");
             int MaxNumber = Check.MaximumInteger(11, 22, 33);
             Console.WriteLine(MaxNumber);
+
+            float MaxNumber1 = Check.MaximumFloat(11.2f,45.2f,45.63f);
+            Console.WriteLine(MaxNumber1);
         }
     }
 }
