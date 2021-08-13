15833feb0003c38aa69d81cd5110ed8bdfc3e13e
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-13 10:33:37 +0530

    feat:Using Generic Method

--- FindMaximumNum/GenericMax.cs
+++ FindMaximumNum/GenericMax.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace FindMaximumNum
 {
-    class GenericMax
+    public class GenericMax<T> where T : IComparable
     {
+        public T[] value;
+        public GenericMax(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void printmaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum value is " + max);
+        }
     }
 }
--- FindMaximumNum/Program.cs
+++ FindMaximumNum/Program.cs
@@ -9,14 +9,26 @@ namespace FindMaximumNum
             Console.WriteLine("Welcome to Maximum Number Program ! ");
 
             Console.WriteLine(" Maximum Number is:-");
-            int MaxNumber = Check.MaximumInteger(11, 22, 33);
-            Console.WriteLine(MaxNumber);
+            int[] intArray = { 112, 344, 432, 555, 678 };
+            GenericMax<int> generic = new GenericMax<int>(intArray);
+            generic.printmaxValue();
 
-            float MaxNumber1 = Check.MaximumFloat(11.2f,45.2f,45.63f);
-            Console.WriteLine(MaxNumber1);
+            float[] floatArray = { 112f, 344f, 432f, 555f, 678f };
+            GenericMax<float> genericdouble = new GenericMax<float>(floatArray);
+            generic.printmaxValue();
 
-            string MaxNumber2 = Check.MaximumString("42.2","45.12","45.25");
-            Console.WriteLine(MaxNumber2);
+            string[] stringArray = { "112", "344", "432", "555", "678" };
+            GenericMax<string> genericstring = new GenericMax<string>(stringArray);
+            generic.printmaxValue();
+
+            //int MaxNumber = Check.MaximumInteger(11, 22, 33);
+            //Console.WriteLine(MaxNumber);
+
+            //float MaxNumber1 = Check.MaximumFloat(11.2f,45.2f,45.63f);
+            //Console.WriteLine(MaxNumber1);
+
+            //string MaxNumber2 = Check.MaximumString("42.2","45.12","45.25");
+            //Console.WriteLine(MaxNumber2);
         }
     }
 }
