4173638e355de202d378a7d5f4fec095872e9bde
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-13 10:19:27 +0530

    feat:FindMaximumString

---
+++ FindMaximumNum/Check.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMaximumNum
+{
+    class Check
+    {
+        public static int MaximumInteger(int firstValue, int secondValue, int thirdValue)
+        {
+
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            else if (thirdValue.CompareTo(secondValue) > 0 && thirdValue.CompareTo(firstValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstValue,secondValue,Thirdvalue are same !");
+
+        }
+    }
+}
---
+++ FindMaximumNum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FindMaximumNum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Number Program");
+
+            Console.WriteLine(" Maximum Number is :-");
+            int MaxNumber = Check.MaximumInteger(11, 22, 33);
+            Console.WriteLine(MaxNumber);
+        }
+    }
+}
